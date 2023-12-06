17c285ce5a92daa7b4c193a330ab44e64c1e7101
이수정 <developercosj@gmail.com>
2023-12-06 17:24:27 +0900

    [이수정] JPA 초기 설정 및 스프링부트 없이 JPA 기본 사용 테스트
    - 인프런 강의 자바 ORM 표준 JPA 프로그래밍 - 기본편 공부

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,55 @@
+package org.example;
+
+import hellojpa.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+            // insert
+            //Member member = new Member();
+            //member.setId(1L);
+            //member.setName("memberName");
+
+            //em.persist(member);
+
+            // select
+            Member findMember = em.find(Member.class, 1L);
+
+            // update
+            // 컬렉션을 관리하는 것처럼 생각할것
+            // JPA 를 통해서 객체를 가지고 오면 트랜잭션을 커밋하는 시점에 변경사항을 확인하여 변경사항은 update 해줌
+            findMember.setName("HelloJpa");
+
+            // delete
+            //em.remove(findMember);
+
+            // JPQL 사용
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(8)
+                    .getResultList();
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+
+
+
+    }
+}
