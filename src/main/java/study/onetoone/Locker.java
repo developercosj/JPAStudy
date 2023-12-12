a95cd77f48ed0b4b348fc028ef6703f7eb38706b
이수정 <developercosj@gmail.com>
2023-12-13 01:41:19 +0900

    [이수정] 1:1 관계 예시 - 1:1 의 관계시 한쪽은 읽기 전용으로 설정하고 주 테이블에 단방향으로 외래키 설정 예시

---
+++ src/main/java/study/onetoone/Locker.java
@@ -0,0 +1,23 @@
+package study.onetoone;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Locker {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+
+    // 읽기 전용
+    @OneToOne(mappedBy = "locker_id")
+    private Member member;
+
+
+}
