c767c82067ae0c1bfee131fe628334bcb7b65155
이수정 <developercosj@gmail.com>
2023-12-12 15:34:25 +0900

    [이수정] 클래스 이름 변경

---
+++ src/main/java/study/relationStudy/RelationStudyController.java
@@ -0,0 +1,67 @@
+package study.relationStudy;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class RelationStudyController {
+
+
+    public void relationTest() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            TeamRelationEntity team = new TeamRelationEntity();
+            team.setName("TeamA");
+            //team.getMembers().add(member);
+            em.persist(team);
+
+            MemberRelationEntity member = new MemberRelationEntity();
+            member.setUsername("member1");
+            // 연관관계의 주인에게 값을 넣음
+            member.changeTeam(team);
+            em.persist(member);
+
+            team.addMember(member);
+
+
+
+
+            //DB 를 commit() 하기 전에 SQL DB에 실행
+            em.flush();
+            // 영속성컨텍스트를 지우기 때문에 아래 find() 는 DB 에서 가지고 올 수 있도록 함
+            em.clear();
+
+            MemberRelationEntity findMember = em.find(MemberRelationEntity.class, member.getId());
+            List<MemberRelationEntity> members = findMember.getTeam().getMembers();
+
+            for (MemberRelationEntity member1 : members) {
+                System.out.println("m = " + member.getUsername());
+
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+
+
+
+
+
+     }
+
+
+
+
+}
---
+++ src/main/java/study/relationStudy/TeamRelationEntity.java
@@ -0,0 +1,49 @@
+package study.relationStudy;
+
+
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class TeamRelationEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "team_id")
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<MemberRelationEntity> members = new ArrayList<>();
+
+    public void addMember (MemberRelationEntity member) {
+        member.setTeam(this);
+        members.add(member);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<MemberRelationEntity> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<MemberRelationEntity> members) {
+        this.members = members;
+    }
+}
