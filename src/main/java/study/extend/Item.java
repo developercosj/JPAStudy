c81703e74164992a3cb842fb67e9b26963b27283
이수정 <developercosj@gmail.com>
2023-12-13 16:48:50 +0900

    [이수정] 상속관계 매핑 중 InheritanceType.TABLE_PER_CLASS
    - @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS) 작성
    - @DiscriminatorColumn이 필요 없음
    - 부모 객체로 조회시 UNION ALL로 자식 테이블을 모두 다 조회하게 되는 단점이 있음

--- src/main/java/study/extend/Item.java
+++ src/main/java/study/extend/Item.java
@@ -3,9 +3,9 @@ package study.extend;
 import javax.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn
-public class Item {
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+//@DiscriminatorColumn
+public abstract class Item {
 
     @Id @GeneratedValue
     private Long id;
--- src/main/java/study/extend/Movie.java
+++ src/main/java/study/extend/Movie.java
@@ -4,7 +4,7 @@ import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
-@DiscriminatorValue("Movie")
+//@DiscriminatorValue("Movie")
 public class Movie extends Item {
 
     private String director;
